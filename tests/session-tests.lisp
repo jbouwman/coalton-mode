0d8a3f0b2b419aad3b294d4beec743e9bfea74b7
Jesse Bouwman <jlbouwman@hrl.com>
2024-10-16 11:12:13 -0700

    additional server capabilities

--- tests/session-tests.lisp
+++ tests/session-tests.lisp
@@ -1,12 +1,13 @@
 (in-package #:coalton-mode/tests)
 
+(defun send-message (session message)
+  (cm::message-value
+   (cm::process-request session (cm::new-message 'cm::request-message message))))
+
+
 (deftest initialize-session ()
   (let ((session (make-instance 'cm::session)))
-    (is (equal (slot-value
-                (cm::process-request
-                 session
-                 (cm::new-message 'cm::request-message coalton-mode/examples:initialize))
-                'cm::value)
+    (is (equal (send-message session coalton-mode/examples:initialize)
                '((:RESULT
                   (:CAPABILITIES
                    (:POSITION-ENCODING . "utf-16")
@@ -15,3 +16,6 @@
                    (:NAME . "Coalton")))
                  (:ID . 1)
                  (:JSONRPC . "2.0"))))))
+
+
+(send-message (make-instance 'cm::session) coalton-mode/examples:initialize)
